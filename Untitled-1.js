3154424a1c933080ebb06164d926fca215671474
shivani <jaiswalshivani708@gmail.com>
2021-12-04 16:04:54 +0530

    added all the file in git

---
+++ Untitled-1.js
@@ -0,0 +1,5 @@
+
+do { console.log("hii"); } while(i<5);
+var i=0; while(i<5); { console.log("hii"); ++i; }
+for(i=0, j=10; i<5; ++i) { if(i<5) {break;} console.log("hii"); }
+var i=0; while(i<5) { console.log("hii"); ++i; }
